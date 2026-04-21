5b48f01a78df92c72200589ae1c346943bffa46b
enitzsche <165913884+enitzsche@users.noreply.github.com>
2026-04-20 22:32:17 -0400

    Add files via upload

---
+++ Road.java
@@ -0,0 +1,13 @@
+package project5;
+
+class Road {
+    int end1;
+    int end2;
+    int distance;
+
+    Road(int end1, int end2, int distance) {
+        this.end1 = end1;
+        this.end2 = end2;
+        this.distance = distance;
+    }
+}
---
+++ RoadsScholar.java
@@ -0,0 +1,21 @@
+
+// n = the number of intersections 1.
+// m = the number of roads between connections 2.
+// k = the number of intersections that are also cities 3.
+package project5;
+import java.util.Scanner;
+public class RoadsScholar {
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // get values for n, m, and k
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            
+        }
+}
+}
