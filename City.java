8c0e1ddb5676dccee52a56a1182c935f1b75f363
Andrew McDougall <andrewmcdougall@cedarville.edu>
2026-04-22 15:37:22 -0400

    Add City class with constructor and properties

---
+++ City.java
@@ -0,0 +1,11 @@
+package project5;
+
+public class City {
+     String name;
+     int intersection;
+
+     City(int inter, String nm) {
+          name = nm;
+          intersection = inter;
+     }
+}
