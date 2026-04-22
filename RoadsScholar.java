ada2b7ecd107f2cda6881e0626d7ce0757b22e76
Andrew McDougall <andrewmcdougall@cedarville.edu>
2026-04-22 15:38:06 -0400

    Enhance RoadsScholar to manage roads and cities
    Added road and city maps to store roads and cities. Implemented logic to read signs and update road properties accordingly.

--- RoadsScholar.java
+++ RoadsScholar.java
@@ -1,8 +1,5 @@
-
-// n = the number of intersections 1.
-// m = the number of roads between connections 2.
-// k = the number of intersections that are also cities 3.
 package project5;
+
 import java.util.Scanner;
 public class RoadsScholar {
     
@@ -13,9 +10,29 @@ public class RoadsScholar {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int k = scanner.nextInt();
+        Road[] roadMap = new Road[m];
+        City[] cityMap = new City[k];
 
         for (int i = 0; i < m; i++) {
             Road road = new Road(scanner.nextInt(), scanner.nextInt(), scanner.nextFloat());
+            roadMap[i] = road;
         }
-}
+        for(int i = 0; i < k; i++) {
+          City city = new City(scanner.nextInt(), scanner.nextLine());
+          cityMap[i] = city;
+        }
+
+        int numSigns = scanner.nextInt();
+
+        for(int i = 0; i < numSigns; i++) {
+          int signEnd1 = scanner.nextInt();
+          int signEnd2 = scanner.nextInt();
+          for(int j = 0; j < roadMap.length; j++) {
+               if(roadMap[j].end1 == signEnd1 && roadMap[j].end2 == signEnd2) {
+                    roadMap[j].signToPlace = true;
+                    roadMap[j].signDistance = scanner.nextFloat();
+               }
+          }
+        }
+     }
 }
